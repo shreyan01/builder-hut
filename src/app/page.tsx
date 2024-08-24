24d60659a903c34f5f74aa9f36ae5bc961b30929
shreyan01 <shreyanshukla19@gmail.com>
2024-08-24 23:34:08 +0530

    added gform link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
+'use client'
 import Image from "next/image";
 import favicon from './../../public/favicon.png';
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -9,7 +11,11 @@ export default function Home() {
         <p className="text-xl font-normal">building a place where people can build cool shit because I got inspired by buildspace</p>
         <Image src={favicon} alt="maintext" width={300} height={300}/>
       </div>
-        <button className=" flex flex-row w-1/4 text-2xl font-bold text-white p-5 items-center justify-center bg-black rounded-2xl">join builderhut</button>
+        <Link href="https://forms.gle/SNnZVLBNxK1CiTzMA" className=" flex flex-row w-1/4 text-2xl font-bold text-white p-5 items-center justify-center bg-black rounded-2xl shadow-lg backdrop-blur-lg drop-shadow-lg hover:bg-gray-800 active:shadow-none">
+        <button >
+        join builderhut
+        </button>
+        </Link>
     </main>
   );
 }
