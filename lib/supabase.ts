87b1495a6c7c7f93d84aa1cc6f0a15dcfb1c24b9
shreyan01 <shreyanshukla19@gmail.com>
2024-11-30 00:07:56 +0530

    test commit

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -4,6 +4,5 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 console.log('Supabase URL:', supabaseUrl);
-console.log('Supabase Anon Key:', supabaseKey);
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
